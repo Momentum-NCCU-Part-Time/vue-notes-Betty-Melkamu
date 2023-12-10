ef5762377466dd04720b319127112b5d9281aae8
Betty Melkamu <betealmelkamu@gmail.com>
2023-12-10 16:29:51 -0500

    Note Component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
-
-fetch('http://localhost:3000/notes/')
-
+//We need to do this at the mount of the site, so we can make notes
+import { ref } from "vue";
+import Note from "./assets/components/note.vue";
+const notesList = ref([]);
+//Fetch from database
+fetch("http://localhost:3000/notes/", {
+  method: "GET",
+})
+  .then((res) => res.json())
+  .then((data) => (notesList.value = data));
 </script>
 
 <template>
   <div>
-    <h1>Vue Notes</h1>
+    <h1>Betty's Vue Notes App</h1>
+    <ul>
+      <li class="noteItem" v-for="noteItem in notesList">
+        <Note v-model:title="noteItem.title" v-model:content="noteItem.body" />
+
+      </li>
+    </ul>
+    <button>Add Note</button>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/assets/components/note.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  content: String,
+});
+
+function deleteNote() {
+fetch("http://localhost:3000/notes/", {
+  method: "DELETE",
+})
+  .then((res) => res.json())
+  .then((data) => (notesList.value = data));
+}
+</script>
+
+<template class="note">
+  <h2>{{ props.title }}</h2>
+  <p>{{ props.content }}</p>
+  <button @click = >Edit</button>
+  <button @click = "" >Delete</button>
+</template>
