4ad3bf7940ddc41ff732a8609121dd40453b5247
Betty Melkamu <betealmelkamu@gmail.com>
2023-12-10 18:13:28 -0500

    Get and Post in progress

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
+
 //We need to do this at the mount of the site, so we can make notes
+
 import { ref } from "vue";
 import Note from "./assets/components/note.vue";
 const notesList = ref([]);
+
 //Fetch from database
 fetch("http://localhost:3000/notes/", {
   method: "GET",
 })
   .then((res) => res.json())
   .then((data) => (notesList.value = data));
+
+//Post New Note to Database
+
+function createNote() {
+fetch("http://localhost:3000/notes/", {
+  method: "POST",
+  headers: {"Content-Type": "application/json"}, 
+  body: JSON.stringify({ title, body, updatedAt: new Date() })
+})
+  .then((res) => res.json())
+  .then((data) => (notesList.value = data));
+}
 </script>
 
 <template>
@@ -16,11 +31,10 @@ fetch("http://localhost:3000/notes/", {
     <h1>Betty's Vue Notes App</h1>
     <ul>
       <li class="noteItem" v-for="noteItem in notesList">
-        <Note v-model:title="noteItem.title" v-model:content="noteItem.body" />
-
+        <Note v-model:title="noteItem.title" v-model:content="noteItem.body" v-model:id="noteItem.id" />
       </li>
     </ul>
-    <button>Add Note</button>
+    <button @click="createNote()" type="submit">Add Note</button>
   </div>
 </template>
 
--- src/assets/components/note.vue
+++ src/assets/components/note.vue
@@ -1,21 +1,22 @@
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   title: String,
   content: String,
+  id: Number,
 });
 
 function deleteNote() {
-fetch("http://localhost:3000/notes/", {
-  method: "DELETE",
-})
-  .then((res) => res.json())
-  .then((data) => (notesList.value = data));
+  fetch("http://localhost:3000/notes/" + props.id, {
+    method: "DELETE",
+  });
 }
 </script>
 
 <template class="note">
   <h2>{{ props.title }}</h2>
   <p>{{ props.content }}</p>
-  <button @click = >Edit</button>
-  <button @click = "" >Delete</button>
+  <button @click="">Edit</button>
+  <button type="submit" @click="deleteNote()">Delete</button>
 </template>
